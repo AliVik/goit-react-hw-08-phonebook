e0394feac9acabfd5753f52bb837c4860def875d
AliVik <85441270+AliVik@users.noreply.github.com>
2022-05-03 23:07:11 +0300

    change index files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 export { default as ContactsPage } from './ContactsPage';
 export { default as RegisterPage } from './RegisterPage';
 export { default as LoginPage } from './LoginPage';
-export { default as MainPage } from './MainPage';
+export { default as Layout } from './Layout';
