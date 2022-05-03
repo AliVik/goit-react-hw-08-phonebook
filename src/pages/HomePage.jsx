8ac99cc1b6750042931008fadb011a51188beb83
AliVik <85441270+AliVik@users.noreply.github.com>
2022-05-03 23:06:44 +0300

    add Login and HomePage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import {Wrapper} from './StyledLayout'
+
+export default function HomePage(){
+    return(
+        <section>
+        <Wrapper>
+          <h1>Welcome to The Web Phonebook App!</h1>
+        </Wrapper>
+      </section>
+    )
+}
