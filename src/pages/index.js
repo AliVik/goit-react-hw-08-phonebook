04c84eec832e9ccafcf51348ba3729b87e0a3568
AliVik <85441270+AliVik@users.noreply.github.com>
2022-05-03 20:27:34 +0300

    add reexport file

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as ContactsPage } from './ContactsPage';
+export { default as RegisterPage } from './RegisterPage';
+export { default as LoginPage } from './LoginPage';
+export { default as MainPage } from './MainPage';
