6fd695011435d0821d52622ace9f5ddc05d6b866
AliVik <85441270+AliVik@users.noreply.github.com>
2022-05-04 18:13:44 +0300

    corrected exports

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store';
-import App from './pages/Layout';
+import App from './App';
 import './index.css';
 
 ReactDOM.render(
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { default as ContactsPage } from './ContactsPage';
 export { default as RegisterPage } from './RegisterPage';
 export { default as LoginPage } from './LoginPage';
 export { default as Layout } from './Layout';
+export { default as HomePage } from './HomePage';
