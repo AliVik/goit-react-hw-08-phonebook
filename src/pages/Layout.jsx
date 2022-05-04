49c995f952bc8488d2258ab1d75d265d3bdc7787
AliVik <85441270+AliVik@users.noreply.github.com>
2022-05-05 00:23:15 +0300

    added LogIn markup

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,16 +1,23 @@
 
 import { Outlet } from 'react-router-dom';
-import {Header,StyledLink} from './StyledLayout';
+import {Header,StyledLink, SuccessLogInWrapper, LogOutButton, GreetingText} from './StyledLayout';
 import {Wrapper} from './StyledLayout'
 
 export default function Layout() {
   return (
     <Wrapper>
       <Header>
+        <div>
         <StyledLink to="/">Home</StyledLink>
         <StyledLink to="/sign_up">Sign up</StyledLink>
         <StyledLink to="/log_in">Log in</StyledLink>
         <StyledLink to="/contacts">Contacts</StyledLink>
+        </div>
+        
+        <SuccessLogInWrapper>
+          <GreetingText>Welcome!You are in</GreetingText>
+          <LogOutButton type="submit">Log out</LogOutButton>
+        </SuccessLogInWrapper>
       </Header>
 
       <Outlet />
