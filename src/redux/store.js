660d2557a8ec1157829fb50dace7e3aac92efab0
AliVik <85441270+AliVik@users.noreply.github.com>
2022-05-04 23:51:17 +0300

    add redux-persist and its logic

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import filterSlice from 'features/filterSlice';
 import { contactsFromApi } from '../features/apiSlice';
 import { setupListeners } from '@reduxjs/toolkit/query';
+import { configure } from '@testing-library/react';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, contactsFromApi.reducer);
 
 export const store = configureStore({
   reducer: {
     filter: filterSlice.reducer,
-    [contactsFromApi.reducerPath]: contactsFromApi.reducer,
+    [contactsFromApi.reducerPath]: persistedReducer,
   },
   middleware: getDefaultMiddleware => [
     ...getDefaultMiddleware(),
     contactsFromApi.middleware,
   ],
 });
+
+export const persistor = persistStore(store);
 
 setupListeners(store.dispatch);
