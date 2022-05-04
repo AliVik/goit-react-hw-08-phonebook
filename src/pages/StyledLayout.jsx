06a9767d8f30ebad9001ee48e9e122ae077223ee
AliVik <85441270+AliVik@users.noreply.github.com>
2022-05-05 00:24:07 +0300

    added styles

--- src/pages/StyledLayout.jsx
+++ src/pages/StyledLayout.jsx
@@ -2,7 +2,11 @@ import styled from "styled-components";
 import { NavLink } from 'react-router-dom';
 
 export const Header = styled.header`
-padding: 20px;
+display: flex;
+padding: 10px;
+justify-content: space-between;
+align-items: center;
+border-bottom: 1px solid grey;
 `
 export const StyledLink = styled(NavLink)`
 display: inline-block;
@@ -16,5 +20,14 @@ color: #000;
 export const Wrapper = styled.div`
 padding: 0 20px 0 20px;
 `
-export const Greeting = styled.h1`
+export const SuccessLogInWrapper = styled.div`
+display: flex;
+`
+export const LogOutButton = styled.button`
+
+padding: 5px 40px;
+border-radius: 5%;
+`
+export const GreetingText = styled.p`
+margin-right: 5px;
 `
