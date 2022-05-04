e5835552d3f58c6756e46574052eb10f63fa1659
AliVik <85441270+AliVik@users.noreply.github.com>
2022-05-04 18:14:20 +0300

    corrected routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import { render } from 'react-dom';
 import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { ContactsPage, RegisterPage, HomePage, LoginPage, Layout } from 'pages';
 
 const LoginPage = lazy(() => import('pages/LoginPage'));
 const ContactsPage = lazy(() => import('pages/ContactsPage'));
 const RegisterPage = lazy(() => import('pages/RegisterPage'));
-const Layout = lazy(() => import('pages/MainPage'));
+const Layout = lazy(() => import('pages/Layout'));
+const HomePage = lazy(() => import('pages/HomePage'));
 
-render(
-  <BrowserRouter>
+export default function App() {
+  return (
     <Suspense fallback="">
       <Routes>
-        <Route path="/" element={<Layout />} />
-        <Route path="sign-up-form" element={<RegisterPage />} />
-        <Route path="log-in-form" element={<LoginPage />} />
-        <Route path="contacts" element={<ContactsPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="sign_up" element={<RegisterPage />} />
+          <Route path="log_in" element={<LoginPage />} />
+          <Route path="contacts" element={<ContactsPage />} />
+        </Route>
       </Routes>
     </Suspense>
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+  );
+}
