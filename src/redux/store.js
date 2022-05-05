2e964bd030bec82f0881a423f43ad6c91d65a043
AliVik <85441270+AliVik@users.noreply.github.com>
2022-05-05 16:58:58 +0300

    correcred redux-store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,27 +1,41 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import filterSlice from 'features/filterSlice';
-import { contactsFromApi } from '../features/apiSlice';
 import { setupListeners } from '@reduxjs/toolkit/query';
-import { configure } from '@testing-library/react';
+import authReducer from '../features/authSlice';
 
 const persistConfig = {
-  key: 'root',
+  key: 'auth',
   storage,
 };
 
-const persistedReducer = persistReducer(persistConfig, contactsFromApi.reducer);
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
-    filter: filterSlice.reducer,
-    [contactsFromApi.reducerPath]: persistedReducer,
+    // filter: filterSlice.reducer,
+    auth: persistedReducer,
   },
-  middleware: getDefaultMiddleware => [
-    ...getDefaultMiddleware(),
-    contactsFromApi.middleware,
-  ],
+  // middleware: getDefaultMiddleware => [
+  //   ...getDefaultMiddleware(),
+  //   auth.middleware,
+  // ],
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
