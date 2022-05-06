843b4b1e832f988fb54a4b505c49329bc786eb6e
AliVik <85441270+AliVik@users.noreply.github.com>
2022-05-06 17:12:05 +0300

    add logout operation in UserMunu

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,11 +1,25 @@
 import {SuccessLogInWrapper, LogOutButton, GreetingText,GreetingTextWrapper} from './StyledUserMenu';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import authSelectors from 'features/authSelectors';
+import { useLogOutUserMutation } from 'features/apiSlice';
+import {logOut} from 'features/authSlice';
 
 
 export default function UserMenu(){
   const userEmail = useSelector(authSelectors.getUserEmail);
   const userName = useSelector(authSelectors.getUserName);
+  const dispatch = useDispatch()
+  const userToken = useSelector(authSelectors.getUserToken);
+
+  const [logOutUser] = useLogOutUserMutation();
+
+  const onLogOutBtnSubmit= async () =>{
+  
+   const returnedUser = await logOutUser(userToken, {
+    selectFromResult: ({data}) => data.user
+   })
+  dispatch(logOut(returnedUser));
+  }
   
     return (
      <SuccessLogInWrapper>
@@ -14,7 +28,7 @@ export default function UserMenu(){
         <GreetingText>{userEmail}</GreetingText>
        </GreetingTextWrapper>
        
-        <LogOutButton type="submit">Log out</LogOutButton>
+        <LogOutButton type="submit" onClick={()=>onLogOutBtnSubmit()}>Log out</LogOutButton>
         
       </SuccessLogInWrapper>
        
