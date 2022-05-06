53f559e67a8c4ae372529285fe99ddc481d971be
AliVik <85441270+AliVik@users.noreply.github.com>
2022-05-06 17:09:21 +0300

    add Authorization header in RTK query ApiSlice

--- src/features/apiSlice.js
+++ src/features/apiSlice.js
@@ -1,9 +1,20 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { store } from 'redux/store';
 
 export const userApiSlice = createApi({
   reducerPath: 'auth',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://connections-api.herokuapp.com',
+
+    prepareHeaders: headers => {
+      const token = store.getState().auth.token;
+      console.log(token);
+
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
   }),
   tagTypes: ['users'],
   endpoints: builder => ({
@@ -15,22 +26,26 @@ export const userApiSlice = createApi({
         providesTags: ['users'],
       }),
     }),
-    // logInUser: builder.mutation({
-    //   query: user => ({
-    //     url: `/users/login`,
-    //     method: 'POST',
-    //     body: { email: user.email, password: user.password },
-    //   }),
-    //   providesTags: ['users'],
-    // }),
-    // logOutUser: builder.query({
-    //   query: contactId => ({
-    //     url: `/contacts/${contactId}`,
-    //     method: 'DELETE',
-    //   }),
-    //   invalidatesTags: ['Contacts'],
-    // }),
+    logInUser: builder.mutation({
+      query: user => ({
+        url: `/users/login`,
+        method: 'POST',
+        body: { email: user.email, password: user.password },
+      }),
+      providesTags: ['users'],
+    }),
+    logOutUser: builder.mutation({
+      query: token => ({
+        url: `/users/logout`,
+        method: 'POST',
+      }),
+      providesTags: ['users'],
+    }),
   }),
 });
 
-export const { useSignUpUserMutation, useLogInUserMutation } = userApiSlice;
+export const {
+  useSignUpUserMutation,
+  useLogInUserMutation,
+  useLogOutUserMutation,
+} = userApiSlice;
