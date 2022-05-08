cc7a45051c39224a70ee31fa0c787ea8750a2da5
AliVik <85441270+AliVik@users.noreply.github.com>
2022-05-08 16:09:57 +0300

    create contactApiSlice

---
+++ src/redux/api/contactsApiSlice.js
@@ -0,0 +1,53 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { store } from 'redux/store';
+
+export const contactsApiSlice = createApi({
+  reducerPath: 'contactsApiSlice',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://connections-api.herokuapp.com',
+    prepareHeaders: headers => {
+      const token = store.getState().auth.token;
+
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ['contacts'],
+  endpoints: builder => ({
+    getContacts: builder.query({
+      query: () => `/contacts`,
+      providesTags: ['contacts'],
+    }),
+    postContact: builder.mutation({
+      query: contact => ({
+        url: `/contacts`,
+        method: 'POST',
+        body: contact,
+      }),
+      invalidatesTags: ['contacts'],
+    }),
+    deleteContact: builder.mutation({
+      query: contactId => ({
+        url: `/contacts/${contactId}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['contacts'],
+    }),
+    patchContact: builder.mutation({
+      query: (contactId, patchedContact) => ({
+        url: `/contacts/${contactId}`,
+        method: 'PATCH',
+        body: patchedContact,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetContactsQuery,
+  usePostContactMutation,
+  useDeleteContactMutation,
+  usePatchContactMutation,
+} = contactsApiSlice;
