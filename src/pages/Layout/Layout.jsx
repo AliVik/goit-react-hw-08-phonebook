a0b0178d508f6a74458d13954f800bf9526f30e9
AliVik <85441270+AliVik@users.noreply.github.com>
2022-05-10 18:06:45 +0300

    add condition of rendering pages

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,9 +1,8 @@
-
 import { Outlet } from 'react-router-dom';
-import {Header,StyledLink} from './StyledLayout';
+import { Header, StyledLink } from './StyledLayout';
 import UserMenu from 'components/UserMenu';
 import { useSelector } from 'react-redux';
-import authSelectors from 'redux/auth/authSelectors'
+import authSelectors from 'redux/auth/authSelectors';
 import { useEffect } from 'react';
 import { useGetUserQuery } from 'redux/api/userApiSlice';
 import { refresh } from 'redux/auth//authSlice';
@@ -11,37 +10,38 @@ import { useDispatch } from 'react-redux';
 
 export default function Layout() {
   const dispatch = useDispatch();
-const IsLoggedIn = useSelector(authSelectors.getIsLoggedIn);
-const token = useSelector(authSelectors.getUserToken);
-const {data} =  useGetUserQuery(token, {
-skip: token===null,
-});
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
 
-useEffect(()=> {
-  if(data){
-    dispatch(refresh(data));
-  }
-},[data, dispatch])
+  const token = useSelector(authSelectors.getUserToken);
+  const { data, isFetching } = useGetUserQuery(token, {
+    skip: token === null,
+  });
 
+  useEffect(() => {
+    if (data) dispatch(refresh(data));
+  }, [data, dispatch]);
 
   return (
-    <>
-      <Header>
-        
-        <StyledLink to="/">Home</StyledLink>
-      
-       
-        {IsLoggedIn?
-        <> <StyledLink to="/contacts">Contacts</StyledLink>
-           <UserMenu/>
-        </> :<>
-          <StyledLink to="/register">Sign up</StyledLink>
-          <StyledLink to="/login">Log in</StyledLink>
-        </>}
-     
-      </Header>
+    !isFetching && (
+      <>
+        <Header>
+          <StyledLink to="/">Home</StyledLink>
 
-      <Outlet />
-    </>
+          {isLoggedIn ? (
+            <>
+              <StyledLink to="/contacts">Contacts</StyledLink>
+              <UserMenu />
+            </>
+          ) : (
+            <>
+              <StyledLink to="/register">Sign up</StyledLink>
+              <StyledLink to="/login">Log in</StyledLink>
+            </>
+          )}
+        </Header>
+
+        <Outlet />
+      </>
+    )
   );
 }
