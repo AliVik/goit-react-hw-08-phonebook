02d6051ccb0e8a2ffbf6749e9028e768ccd3c9d2
AliVik <85441270+AliVik@users.noreply.github.com>
2022-06-04 22:27:01 +0300

    add adaptivity, fix styles

--- src/pages/HomePage/StyledHomePage.jsx
+++ src/pages/HomePage/StyledHomePage.jsx
@@ -10,26 +10,40 @@ export const GreetingHeading = styled.h1`
 `;
 
 export const HomePageSection = styled.section`
-  display: flex;
+  @media screen and (min-width: 769px) {
+    display: flex;
+  }
 `;
 
 export const Image = styled.img`
   display: block;
-  position: absolute;
-  top: 0;
-  left: 0;
+  margin: auto;
   width: 100%;
-  height: 92vh;
 `;
 
 export const HomePageImageWrapper = styled.div`
-  position: relative;
-  width: 100vw;
-  margin-left: 20px;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  background-color: #2dc4f9;
+  border-top: 4px solid #1097c7;
+
+  @media screen and (min-width: 769px) {
+    padding: 0 20px 0 20px;
+    height: 92.7vh;
+    border-left: 4px solid #1097c7;
+  }
 `;
 
 export const HomePageWordsWrapper = styled.div`
-  padding: 150px 0 0 80px;
-  margin-left: auto;
-  margin-right: auto;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  padding: 20px 10px 20px 10px;
+  border-bottom: 4px solid #ffff00;
+
+  @media screen and (min-width: 769px) {
+    padding: 0 20px 0 20px;
+    border-right: 4px solid #ffff00;
+  }
 `;
